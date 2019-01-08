2e775f7864df8896aa03c498e8265ce9202b46d1
guangshu.xu <343389025@qq.com>
2019-01-08 20:19:55 +0800

    add full_reduction

--- 40_dynamic_programming/01_knapsack.py
+++ 40_dynamic_programming/01_knapsack.py
@@ -58,6 +58,40 @@ def bag_with_max_value(items_info:List[Tuple[int, int]], capacity:int) -> int:
                     )
     return max(memo[-1])
 
+def full_reduction(items:List[int], w:int):
+    """
+    淘宝双十一购物节满减活动比如‘满200￥减50￥’.假设女友购物车有 n 个（n>100）个想买的商品，她希望从中选几个，
+    在凑够满减条件下，让选出来的商品价格总和最大程度接近满减条件（200￥）。
+    :param items: n 件商品各自的价格
+    :param w: 满减条件
+    :return: 购买那些商品
+    """
+    n = len(items)
+    memo = [[0]*(3*w+1) for i in range(n)]
+    memo[0][0] = 1
+    if items[0] <= 3*w:
+        memo[0][items[0]] = 1
+    for i in range(1, n):
+        for j in range(3*w+1):
+            if memo[i-1][j] != 0:
+                memo[i][j] = memo[i-1][j]
+                if items[i]+j <= 3*w:
+                    memo[i][items[i]+j] = 1
+    tmp = 0
+    for j in  range(w, 3*w+1):
+        if memo[n-1][j] == 1:
+            tmp = j
+            break
+    if tmp == 0:
+        return []
+    select:List[int] = []
+    for i in range(n-1, 0, -1):
+        if (tmp-items[i] >= 0) and (memo[i-1][tmp-items[i]] == 1):
+            select.append((i, items[i]))
+            tmp = tmp - items[i]
+    if tmp > 0:
+        select.append((0, items[0]))
+    return select
 
 if __name__=="__main__":
     # [weight, ...]
@@ -69,5 +103,12 @@ if __name__=="__main__":
     items_info = [(3, 5), (2, 2), (1, 4), (1, 2), (4, 10)]
     capacity = 8
     print(bag_with_max_value(items_info, capacity))
+
+    # 淘宝双十一购物满减活动
+    items:List[int] = [123,88,56,74,89,136,99,211]
+    select = full_reduction(items, 200)
+    total_price = sum([i[1] for i in select])
+    print(select)
+    print(total_price)
 
 
